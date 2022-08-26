a2c094dfeb5038ef7cfd28d763a30ebcf8189253
marawanthedev <tmarawan11@gmail.com>
2022-08-26 10:23:53 +0100

    This commit will remove uneeded modules that were at budgee

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import "./App.css";
 import Landing from "./pages/landing/landing";
-import Dashboard from "./pages/dashboard/dashboard";
-import Login from "./pages/login/login";
 import { useState } from "react";
 import { Route } from "react-router-dom";
 function App() {
-  const [isInAuth, setIsInAuth] = useState(false);
   return (
     <div className="App">
       <Route exact path="/" component={Landing} />
-      <Route
-        path="/dashboard"
-        render={(props) => <Dashboard {...props} isInAuth={isInAuth} />}
-      />
-      <Route
-        path="/login"
-        render={(props) => <Login {...props} setAuthState={setIsInAuth} />}
-      />
     </div>
   );
 }
